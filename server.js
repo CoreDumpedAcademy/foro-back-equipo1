85aa763dfdf83ac12e39f0064d314ab878e26280
Zatapo <peterlapatatita@gmail.com>
2019-12-07 21:20:39 +0100

    fix

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = require('./app');
 
 mongoose.set('useCreateIndex', true);
 
-const mongodb = process.env.MONGODB || 'mongodb://localhost:3000/forodb';
+const mongodb = process.env.MONGODB || 'mongodb://localhost:27017/forodb';
 const port = process.env.PORT || 3000;
 
-mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology, }, err => {
+mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
     if (err) { console.log(`ERROR: connecting to Database. ${err}`); }
-    else app.listen(port, console.log(`API started on http://localhost${port}`));
+    else app.listen(port, console.log(`API started on http://localhost:${port}`));
 })

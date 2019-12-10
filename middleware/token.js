4c2b83b7194a61bd852c978ff29f16174cf09637
blitty <blittynet@gmail.com>
2019-12-11 00:21:55 +0100

    validateToken

--- middleware/token.js
+++ middleware/token.js
@@ -1,14 +1,28 @@
 /* eslint-disable no-console */
+require('../config/config');
+
 const jwt = require('jsonwebtoken');
 
 // generate token
 const sign = (payload) => {
-  jwt.sign(payload, process.env.SECRET_TOKEN, (token, err) => {
+  // eslint-disable-next-line max-len
+  jwt.sign(payload, process.env.SECRET_TOKEN, { expiresIn: process.env.CADUCIDAD_TOKEN }, (token, err) => {
     if (err) return console.log(err);
     return console.log(token);
   });
 };
 
+// validate token
+const validate = (req, res, next) => {
+  // eslint-disable-next-line consistent-return
+  jwt.verify(req.body.token, process.env.SECRET_TOKEN, (jwtErr, decodedToken) => {
+    if (jwtErr) return res.status(401).send({ message: 'Error', jwtErr });
+    req.body.decodedToken = decodedToken;
+    next(); // next function
+  });
+};
+
 module.exports = {
   sign,
+  validate,
 };

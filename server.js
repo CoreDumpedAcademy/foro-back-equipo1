87a7402ba47a47241ee9389bec80407656d04044
joniesp <jonialcala@gmail.com>
2019-12-13 11:25:24 +0100

    Arreglando problemas

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
+/* eslint-disable no-console */
+require('./config/config');
+
 const mongoose = require('mongoose');
 const app = require('./app');
 
 mongoose.set('useCreateIndex', true);
 
-const mongodb = process.env.MONGODB || 'mongodb://localhost:27017/forodb';
-const port = process.env.PORT || 3000;
-
-mongoose.connect(mongodb, { useNewUrlParser: true, useUnifiedTopology: true }, err => {
-    if (err) { console.log(`ERROR: connecting to Database. ${err}`); }
-    else app.listen(port, console.log(`API started on http://localhost:${port}`));
-})
+// eslint-disable-next-line max-len
+mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) console.log(`ERROR: connecting to Database. ${err}`);
+  else app.listen(process.env.PORT, console.log(`\nAPI started on http://localhost:${process.env.PORT}`));
+});

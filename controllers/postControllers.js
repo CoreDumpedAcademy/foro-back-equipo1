9ae3473d2d946129225ef07adbc73774f77b1e11
Zatapo <peterlapatatita@gmail.com>
2019-12-13 19:51:49 +0100

    getPosts

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -9,15 +9,20 @@ const create = (req, res) => {
   });
 };
 
-const getAll = (req, res) => {
+const getPosts = (req, res) => {
+  const page = Number(req.body.page);
+  let limitN = 10;
+  const skip = limitN * (page - 1);
+  limitN *= page;
+
   PostSchema.find({}, (err, users) => {
     if (err) { return res.status(500).send({ message: 'Error', err }); }
 
     return res.status(200).send(users);
-  });
+  }).sort({ date: -1 }).limit(limitN).skip(skip);
 };
 
 module.exports = {
   create,
-  getAll,
+  getPosts,
 };

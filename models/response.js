65947f5945ab9e19833832f4eafba9cb2d485647
blitty <blittynet@gmail.com>
2019-12-15 14:23:25 +0100

    response to a post

--- models/response.js
+++ models/response.js
@@ -6,7 +6,7 @@ const responsePost = new Schema({
   idPost: { type: String }, // asociar con el hilo al que se ha respondido
   bodyText: { type: String, required: [true, 'Body text is required'] },
   date: { type: Date, default: Date.now() },
-  userName: { type: String }, // quien ha escrito la respuesta
+  userNameCreator: { type: String }, // quien ha escrito la respuesta
 });
 
 module.exports = mongoose.model('responsePostHackathonTeam', responsePost);
